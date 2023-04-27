6b955587bd0179843d3498d7c688feac444d5c56
Mateus C Godoy <mateuscgodoy@gmail.com>
2023-04-27 11:20:20 -0300

    Add JavaScript Effects
    Completed the Dark Mode effect on the entire page, as well as the effect
    added to the header that switches from Full name to Initials only when
    the user scroll. Did some minor adjustments to the About Me text too.

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,24 +1,81 @@
-const showLess = (event) => {
-  const liElement = event.target.parentElement;
+// Function Definitions
+const showLessEffect = (button) => {
+  const liElement = button.parentElement;
+  const isDarkMode = liElement.classList.contains("dark-mode");
+  console.log(isDarkMode);
+
   liElement.querySelector(".li-title").style.display = "block";
   liElement.querySelector(".show-section").style.display = "none";
-  liElement.style.backgroundColor = "hsla(0, 0%, 100%, 1)";
-  event.target.innerHTML = "Show More";
-  event.target.onclick = showMore;
+
+  liElement.style.backgroundColor = isDarkMode ? "black" : "white";
+  button.innerHTML = "Show More";
+  button.onclick = showMore;
+};
+
+const showLess = (event) => {
+  showLessEffect(event.target);
 };
 
 const showMore = (event) => {
   const liElement = event.target.parentElement;
+  const isDarkMode = liElement.classList.contains("dark-mode");
+
   liElement.querySelector(".li-title").style.display = "none";
   liElement.querySelector(".show-section").style.display = "block";
-  liElement.style.backgroundColor = "hsla(0, 0%, 90%, 0.5)";
+
+  liElement.style.backgroundColor = isDarkMode
+    ? "var(--low-black)"
+    : "var(--low-white)";
   event.target.innerHTML = "Show Less";
   event.target.onclick = showLess;
 };
 
-const showMoreButtons = Array.from(
-  document.getElementsByClassName("li-button")
-);
-showMoreButtons.forEach((button) => {
-  button.onclick = showMore;
-});
+const assignPageButtons = () => {
+  const showMoreButtons = Array.from(
+    document.getElementsByClassName("li-button")
+  );
+  showMoreButtons.forEach((button) => {
+    button.onclick = showMore;
+  });
+
+  btn = document.getElementById("dark-mode-btn");
+  btn.addEventListener("click", toggleDarkMode);
+};
+
+const headerEffect = () => {
+  const yRangeToActivate = 400;
+  const h2FromHeader = document.getElementById("name-header");
+  if (window.scrollY > yRangeToActivate) {
+    h2FromHeader.innerHTML = "M. C. G.";
+  } else {
+    h2FromHeader.innerHTML = "Mateus Colombo Godoy";
+  }
+};
+
+const toggleDarkMode = () => {
+  document.querySelector("body").classList.toggle("dark-mode");
+  document.querySelector("main").classList.toggle("dark-mode");
+  document.querySelector("footer").classList.toggle("dark-mode");
+  document.querySelector("header").classList.toggle("dark-mode");
+  const subSections = document.querySelectorAll(".sub-section");
+  Array.from(subSections).forEach((x) =>
+    Array.from(x.querySelectorAll("li")).forEach((y) => {
+      y.classList.toggle("dark-mode");
+      const cardIsShowing =
+        y.querySelector(".show-section").style.display === "block";
+      // console.log(y.querySelector(".show-section").style.display);
+      if (cardIsShowing) {
+        showLessEffect(y.querySelector(".li-button"));
+      } else {
+        y.style.backgroundColor = y.classList.contains("dark-mode")
+          ? "black"
+          : "white";
+      }
+    })
+  );
+};
+
+// Function Callings
+assignPageButtons();
+headerEffect();
+window.addEventListener("scroll", headerEffect);
